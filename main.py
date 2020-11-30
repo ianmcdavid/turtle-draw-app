159e1d83c1609e6d2312b10210b8c605794578bf
EMcDz <ianmcdavid25@gmail.com>
2020-11-30 06:11:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import turtle as turtle
+import random
+
+print("Welcome to your drawing app! Select a shape you want to draw. After typing the number and hitting enter, go to the result tab!")
+
+turtle.penup()
+turtle.left(90)
+turtle.forward(250)
+turtle.left(90)
+turtle.forward(300)
+turtle.pendown()
+
+turtle.write("Drawing App!", font=("Arial", 16, "normal"))
+
+turtle.penup()
+turtle.left(90)
+turtle.forward(50)
+turtle.pendown()
+
+turtle.write("To draw a shape, go to the Console tab and choose an option!", font=("Arial", 16, "normal"))
+
+turtle.penup()
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.pendown()
+
+def star():
+  turtle.forward(110)
+  turtle.right(216)
+  turtle.forward(110)
+  turtle.right(216)
+  turtle.forward(110)
+  turtle.right(216)
+  turtle.forward(110)
+  turtle.right(216)
+  turtle.forward(110)
+  # Star
+
+def square():
+  turtle.forward(100)
+  turtle.right(90)
+  turtle.forward(100)
+  turtle.right(90)
+  turtle.forward(100)
+  turtle.right(90)
+  turtle.forward(100)
+  # Square
+
+def hexagon():
+  turtle.forward(100)
+  turtle.right(60)
+  turtle.forward(100)
+  turtle.right(60)
+  turtle.forward(100)
+  turtle.right(60)
+  turtle.forward(100)
+  turtle.right(60)
+  turtle.forward(100)
+  turtle.right(60)
+  turtle.forward(100)
+  # Hexagon
+
+selection = input("1. Star\n2. Square\n3. Hexagon\nSelect a number: ")
+if selection == "1":
+  print("Excellent choice! Go to the result tab to see your creation.")
+  star()
+elif selection == "2":
+  print("Excellent choice! Go to the result tab to see your creation.")
+  square()
+elif selection == "3":
+  print("Excellent choice! Go to the result tab to see your creation.")
+  hexagon()
